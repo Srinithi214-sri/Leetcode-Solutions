a7672fcad0de9369ad81cc8cf4522f68febee168
Srinithi214-sri <premak602@gmail.com>
2026-06-12 21:40:34 -0700

    Solution added

---
+++ customer wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int s=0;
+       
+        for(int i=0;i<accounts.length;i++) {
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++) {
+                sum+=accounts[i][j];
+            }
+            s=Math.max(s,sum);
+        }
+        return s;
+    }
+}
