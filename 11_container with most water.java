a297a8c5c17a9223469cc405a8bd57e306a40188
Srinithi214-sri <premak602@gmail.com>
2026-06-23 07:27:46 -0700

    Solution Added

---
+++ 11_container with most water.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int maxarea=0;
+        while(l<r){
+            int hei=Math.min(height[l],height[r]);
+            int w=r-l;
+            maxarea=Math.max(maxarea,hei*w);
+            if(height[l]<height[r]) l++;
+            else r--;
+        }
+        return maxarea;
+    }
+}
