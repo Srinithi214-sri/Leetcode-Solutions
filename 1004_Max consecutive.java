e87e2675791940f92f5c47ed89509049fe5b5e3f
Srinithi214-sri <premak602@gmail.com>
2026-06-25 09:09:15 -0700

    Implement method to find max consecutive 1s

---
+++ 1004_Max consecutive.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int len=0;
+        int max=0;
+        int zero=0;;
+        for(int i=0;i<nums.length;i++) {
+            len=0;
+            for(int j=i;j<nums.length;j++) {
+                if(nums[j]==1) {
+                    len++;
+                }
+                else if(nums[j]==0) {
+                    zero++;
+                    if(zero<=k) {
+                        len++;
+                    }
+                    if(zero>k) {
+                        zero=0;
+                        break;
+                    }
+               }
+            }
+            max=Math.max(max,len);
+    }
+        return max;
+    }
+}
