b1f304cd4c4f872bbe170d95e5dc18fa99f06755
Srinithi214-sri <premak602@gmail.com>
2026-07-01 09:10:48 -0700

    Implement solution for subarrays using Sliding Window

---
+++ 930_Bin Subarray with sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private int atMost(int[] nums, int goal) {
+        if(goal<0) return 0;
+        int l=0;
+        // int r=0;
+        int c=0;
+        int sum=0;
+        for(int r=0;r<nums.length;r++){
+            sum+=nums[r];
+            while(sum>goal) {
+                
+                sum-=nums[l];
+                l++;
+            }
+            c+=(r-l+1);
+        }
+    
+        return c;
+    
+    }
+}
