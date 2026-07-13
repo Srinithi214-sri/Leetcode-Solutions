fdf1eede202bbdaecc45b17b89ac80edc3810d9f
Srinithi214-sri <premak602@gmail.com>
2026-07-13 03:57:55 -0700

    Add permutation  method for integer arrays

---
+++ 46_Permutations.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        solve(nums,0,nums.length-1,res);    
+        return res;
+    }
+    private void solve(int[] nums,int l,int r,List<List<Integer>> res) {
+        if(l==r) {
+          List<Integer> cur=new ArrayList<>();
+          for(int num:nums) {
+            cur.add(num);
+          }
+          res.add(cur);
+          return;
+        }
+        for(int i=l;i<=r;i++) {
+            swap(nums,l,i);
+            solve(nums,l+1,r,res);
+            swap(nums,l,i);
+        }
+
+    }
+    private void swap(int[] nums,int i,int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
