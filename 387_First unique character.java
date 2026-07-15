0579349347befd3dba14cb2daeb3fb3ca91abc5e
Srinithi214-sri <premak602@gmail.com>
2026-07-15 09:25:16 -0700

    Add solution for finding the first unique character

---
+++ 387_First unique character.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+       int[] f=new int[26];
+       for(int i=0;i<s.length();i++) {
+        f[s.charAt(i)-'a']++;
+       }
+       for(int i=0;i<s.length();i++) {
+          char c=s.charAt(i);
+          if(f[c-'a']==1) {
+            return i;
+          }
+       }
+        return -1;
+    }
+}
